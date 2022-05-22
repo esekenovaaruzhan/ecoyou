0e9e18b2537e9e4e6953d48f62999673b05d49c4
esekenovaaruzhan <102858110+esekenovaaruzhan@users.noreply.github.com>
2022-05-22 15:14:45 +0600

    added js animation and new imgs

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 $(".category h1").css("color", "#3e5141");
 $(".category h4").css("color", "#6d8e72");
+if (document.images) {
+  var image1 = new Image();
+  image1.src = "img/slide1.jpg";
+  var image2 = new Image();
+  image2.src = "img/slide2.jpg";
+  var image3 = new Image();
+  image3.src = "img/slide3.jpg";
+  var image4 = new Image();
+  image4.src = "img/slide4.jpg";
+  var image5 = new Image();
+  image5.src = "img/slide5.jpg";
+  var image6 = new Image();
+  image6.src = "img/slide6.jpg";
 
+}
 var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 var alertTrigger = document.getElementById('liveAlertBtn')
 
@@ -21,13 +35,14 @@ if (alertTrigger) {
 };
 var audio = new Audio('video/notify.mp3');
 var audio2 = new Audio('video/notify2.mp3');
-function notify (){
+
+function notify() {
   audio.play();
   let mail =
-  prompt("Хотите подписаться на нашу рассылку?", "Введите свой email");
-  if(mail != null){
+    prompt("Хотите подписаться на нашу рассылку?", "Введите свой email");
+  if (mail != null) {
     audio2.play();
-    alert("Спасибо! Мы прислали вам письмо на" + mail)
+    alert("Спасибо! Мы прислали вам письмо на " + mail)
+  }
 }
-}
-document.getElementById('incentive').addEventListener('mouseenter', notify);
+document.getElementById('incentive').addEventListener('click', notify);
